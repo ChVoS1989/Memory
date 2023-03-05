6bb3b7b2fd9a8501da78e6e02bf0f2e59e44dfa8
Alexander Hergert <alexander.hergert1989@gmail.com>
2023-03-05 18:07:57 +0100

    added a third style and reworked the settings menu

---
+++ README.md
@@ -0,0 +1,11 @@
+# Memorygame
+
+## Todo
+
+1. Make flip animation and flip sound
+2. Make responsive for desktop (bigger cards less gap)
+3. Rework Restart button to reset the field without a refresh
+4. Add a quick mute button => default off
+5. Rework the theme and add nice little shadow effects
+6. Refactor the whole code and comment it out for better readability
+7. More settings features like cards background theme...
--- settings.js
+++ settings.js
@@ -2,6 +2,11 @@ const buttonSettings = document.querySelector('.gear')
 const buttonSettingsClose = document.querySelector('.close')
 const stylesSettings = document.querySelectorAll('.style')
 
+//toggle off the style 2 and 3 on the beginning of game
+//need a loop if more should be added
+stylesSettings[1].classList.toggle('style2');
+stylesSettings[2].classList.toggle('style3');
+
 const settingsOpen = () => {
     main.style.display = 'none';
     footer.style.display = 'none'
@@ -15,16 +20,27 @@ const settingClose = () =>{
 }
 
 const changeStyle1 = () =>{
-    //starting with this style
-    const newStyleElementSibling = document.querySelector("link[href='style2.css']")
-    console.log('changeStyle 1')
-    if(newStyleElementSibling){
-    newStyleElementSibling.remove()
-    }
-    console.log(newStyleElementSibling)
+    //starting with this style and removing other styles to make sure style 1 is active
+    let style2;
+    let style3;
+    //loop over again to remove all additional styles after style1.css back to actie style1 for cards
+    do{
+        style2 = document.querySelector("link[href='style2.css']")
+        if(style2){
+        style2.remove()
+        }
+        style3 = document.querySelector("link[href='style3.css']")
+        if(style3){
+        style3.remove()
+        }
+    }while(style2 || style3);
+    //the more styles would come themore sense it makes to make a loop
     stylesSettings[1].addEventListener('click', changeStyle2)
     stylesSettings[2].addEventListener('click', changeStyle3)
     stylesSettings[0].removeEventListener('click', changeStyle1)
+    stylesSettings[0].classList.add('style1');
+    stylesSettings[1].classList.remove('style2');
+    stylesSettings[2].classList.remove('style3');
 }
 
 const changeStyle2 = () =>{
@@ -33,16 +49,31 @@ const changeStyle2 = () =>{
     const newStyle = document.createElement('link')
     newStyle.setAttribute('rel', 'stylesheet')
     newStyle.setAttribute('href', 'style2.css')
-    console.log('changeStyle 2')
-    newStyleElementSibling.append(newStyle)
-    console.log(newStyleElementSibling)
+    //new styles comes after style 1 and is always the active because last
+    //the more styles would come themore sense it makes to make a loop
+    newStyleElementSibling.append(newStyle) 
     stylesSettings[1].removeEventListener('click', changeStyle2)
     stylesSettings[0].addEventListener('click', changeStyle1)
+    stylesSettings[2].addEventListener('click', changeStyle3)
+    stylesSettings[1].classList.add('style2');
+    stylesSettings[0].classList.remove('style1');
+    stylesSettings[2].classList.remove('style3');
 }
 
 const changeStyle3 = () =>{
-    console.log('changeStyle 3')
-    //switching to style 3
+    const newStyleElementSibling = document.querySelector("link[href='style.css']")
+    const newStyle = document.createElement('link')
+    newStyle.setAttribute('rel', 'stylesheet')
+    newStyle.setAttribute('href', 'style3.css')
+    newStyleElementSibling.append(newStyle)
+    //new styles comes after style 1 and is always the active because last
+    //the more styles would come themore sense it makes to make a loop
+    stylesSettings[2].removeEventListener('click', changeStyle3)
+    stylesSettings[0].addEventListener('click', changeStyle1)
+    stylesSettings[1].addEventListener('click', changeStyle2)
+    stylesSettings[2].classList.add('style3');
+    stylesSettings[1].classList.remove('style2');
+    stylesSettings[0].classList.remove('style1');
 }
 
 //add Listeners in Settings
